2a8e050971e2ffa021cde95394ae6c3caa7103c2
Rushabh Chudasama <61227481+Risshhiii18@users.noreply.github.com>
2020-09-01 15:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,19 +19,40 @@ def predict():
     
     
     
+    df = pd.read_csv('https://raw.githubusercontent.com/Risshhiii18/loan_amount_predictions/master/Train.csv')
+    df = df.drop(['serial number','credit_amount','credit_history','gurantors','housing_type','foreigner','resident_since'], axis = 1)
+    col = []
+    def convo(df):
+        categorical_data = df.select_dtypes(exclude=[np.number])
+        le = LabelEncoder()
+        for i in categorical_data.columns:
+        #print(dfex[i])
+            xe = le.fit_transform(categorical_data[i])
+            col.append(xe)
+    convo(df)
+    categorical_data = df.select_dtypes(exclude=[np.number])
+    non_categorical_data = df.select_dtypes(include=[np.number])
+    xat = pd.DataFrame(np.transpose(col), columns = categorical_data.columns)
+    final_data = pd.concat([xat,non_categorical_data], axis = 1)
+    sc = StandardScaler()
+    test = sc.fit_transform(final_data)
+    
+    
+    
+    
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
 
     
     
     df = pd.DataFrame(final_features)
-    sc = StandardScaler()
-    test = sc.fit_transform(df)
+    pred = sc.transform(final_features)
     
-    prediction = model.predict(test)
     
-    output = round(prediction[0], 2)
-
+    
+    prediction = model.predict(pred)
+    
+    output = round(prediction[0])
 
 
     return render_template('home.html', prediction_text='You can get some of $ {}'.format(output))
